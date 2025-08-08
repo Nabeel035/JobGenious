2af4c63ae04fba14ce794110ff9d9f3ccc3462de
Nabeel035 <imnabeel118@gmail.com>
2025-08-08 15:35:12 +0500

    Make changes in code

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
 import time
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from home_page import HomePage  # make sure this file exists and is correctly named
-import data  # must have JOBGENIOUS_URL and is_url_reachable()
+from home_page import HomePage
+import data  # your data.py file
 
 class TestJobGenious:
-    @classmethod
-    def setup_class(cls):
-        # Optional: Check if URL is reachable
-        if not data.is_url_reachable(data.JOBGENIOUS_URL):
-            raise Exception("URL not reachable!")
-
+    def setup_method(self):
         options = Options()
         options.add_argument("--start-maximized")
-
-        capabilities = DesiredCapabilities.CHROME.copy()
-        capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
-
-        cls.driver = webdriver.Chrome(options=options)
-        cls.driver.get(data.JOBGENIOUS_URL)
-        cls.page = HomePage(cls.driver)
+        service = Service()
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.get("https://www.gojobgenius.com/")
         time.sleep(2)
 
-    def test_title_contains_jobgenious(self):
-        assert "JobGenius" in self.driver.title
-
-    def test_get_started_button_redirects_to_signup(self):
-        self.page.click_get_started()
+    def test_login_button_click(self):
+        home = HomePage(self.driver)
+        home.click_login_button()
+        home.enter_email(data.EMAIL)
+        home.enter_password(data.PASSWORD)
         time.sleep(2)
-        assert "signup" in self.driver.current_url.lower()
+        home.click_Signin_button()
+        time.sleep(5)  # Optional: Replace with assertion
 
-    @classmethod
-    def teardown_class(cls):
-        time.sleep(1)
-        cls.driver.quit()
+    def teardown_method(self):
+        self.driver.quit()
