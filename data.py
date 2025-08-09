c57818b428e2d1864336cd392eb108c0f655ba4f
Nabeel035 <imnabeel118@gmail.com>
2025-08-10 01:37:45 +0500

    Added constants

--- data.py
+++ data.py
@@ -3,3 +3,13 @@
 JOBGENIOUS_URL = "https://www.gojobgenius.com/"
 EMAIL = "imnabeel118@gmail.com"
 PASSWORD = "Mns@123456"
+
+JOB_TITLE = "Developer"
+HIRING_COMPANY = "Step In Cyber"
+WORK_ARRANGEMENT = "Hybrid"
+WORK_LOCATION = "Remote - Anywhere Global"
+EMPLOYMENT_TYPE = "Full-time"
+EXPERIENCE_LEVEL = "Mid Level"
+SALARY_RANGE = "50-80K USD"
+ADDITIONAL_DETAILS = "No any Additional Details"
+
