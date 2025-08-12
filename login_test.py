e9df101445c845f633f10252e7babc86efc91b5a
Nabeel035 <you@example.com>
2025-08-12 11:15:53 +0500

    Tested login of GoJobgenious

---
+++ login_test.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from home_page import HomePage
+from job_description import JobDescriptionPage
+import data
+
+class TestJobGenious:
+    def setup_method(self):
+        options = Options()
+        options.add_argument("--start-maximized")
+        service = Service()
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.get(data.JOBGENIOUS_URL)
+        time.sleep(2)
+
+
+    def test_create_job(self):
+        # Initialize page objects
+        home = HomePage(self.driver)
+        # Call the login method from HomePage
+        home.login(data.EMAIL, data.PASSWORD)
+
+    def teardown_method(self):
+        self.driver.quit()
